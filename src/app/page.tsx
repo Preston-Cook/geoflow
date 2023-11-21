e3a1dd33746618b03b23bf973f5a681ea499e5cd
Preston-Cook <preston.l.cook@gmail.com>
2023-11-21 16:49:51 -0600

    prod test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Page() {
     <>
       <header>
         <h1 className="font-semibold text-4xl text-center text-white bg-[#111827] mt-8 w-fit px-16 py-4 rounded-xl mx-auto md:w-[50%]">
-          🌎 Geoflow
+          🌎 TESTING
           <div>
             <p className="font-normal text-base mt-2 text-gray-500 dark:text-gray-400">
               See Where People Have Visted Your Site!
